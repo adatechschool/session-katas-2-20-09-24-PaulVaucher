d4c13b07a3c9957f313d1f2a37bb3a35768905fd
Paul Vaucher <araghast@gmail.com>
2024-09-20 16:58:40 +0200

    final kata2

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 
 
-function createDeck () {
+function createDeck() {
     // différentes valuers des cartes
     let deck = []
-    const suits = ["♠︎","♣︎","♡","♢"]
+    const suits = ["♠︎", "♣︎", "♡", "♢"]
     const values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
     // distribue les valeurs dans les séries
-    for (let s=0;s<suits.length;s++) {
-        for (let v=0;v<values.length;v++) {
-            let card = {'value':values[v], 'suit':suits[s]};
+    for (let s = 0; s < suits.length; s++) {
+        for (let v = 0; v < values.length; v++) {
+            let card = { 'value': values[v], 'suit': suits[s] };
             deck.push(card);
         }
     }
     console.log(deck)
+    return deck
 }
 
-deck = createDeck ()
+deck = createDeck()
 
 // randomize les cartes (code pompé)
-function shuffleDeck (deck) {
-    for (let i = deck.length - 1; i > 0; i--) { //deck is undefined ?!
+function shuffleDeck(deck) {
+    for (let i = deck.length - 1; i > 0; i--) { //oubli de return...
         const j = Math.floor(Math.random() * (i + 1));
         [deck[i], deck[j]] = [deck[j], deck[i]];
     }
@@ -28,4 +29,8 @@ function shuffleDeck (deck) {
     return deck;
 }
 
-shuffleDeck (deck)
+shuffleDeck(deck)
+
+function deal(dealt) {
+    // pas eu le temps
+}
