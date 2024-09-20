d4c5b2b96cccaf4c57c7401023edd2caafd87ac4
Paul Vaucher <araghast@gmail.com>
2024-09-20 16:13:06 +0200

    shuffle WIP

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
 
 function createDeck () {
+    // différentes valuers des cartes
     let deck = []
     const suits = ["♠︎","♣︎","♡","♢"]
     const values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
+    // distribue les valeurs dans les séries
     for (let s=0;s<suits.length;s++) {
         for (let v=0;v<values.length;v++) {
             let card = {'value':values[v], 'suit':suits[s]};
@@ -14,4 +16,14 @@ function createDeck () {
     console.log(deck)
 }
 
-createDeck ()
+cardDeck = createDeck ()
+
+function shuffleDeck (deck) {
+    startIndex = deck.length
+    randomIndex = Math.random()*startIndex
+    startIndex=randomIndex
+    console.log(startIndex)
+    return startIndex
+}
+
+shuffleDeck (cardDeck)
